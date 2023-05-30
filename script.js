a82e05382b99a224fd1712db5f9a95269dd9eff0
slmshi <sorag0024@gmail.com>
2023-05-30 21:58:42 +0100

    updated with audio file

--- script.js
+++
@@ -1,9 +0,0 @@
-function change(){
-    $.ajax({
-        url: "http://stackoverflow.com",
-        data: { uname: "test" },
-        type: "GET",
-        headers:{Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7"},
-        success: function() { alert('Success!' + authHeader); }
-    });
-}
