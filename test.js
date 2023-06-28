5252e1d674daf82fa0b32ee0fd41ac34712a9f38
slmshi <sorag0024@gmail.com>
2023-06-28 19:46:03 +0100

    add twitter and videos

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
-var topnav = document.getElementsByClassName('topnav')
-addEventListener('click', myfunc())
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
 
-function myfunc()
-{
-    const t2 = document.getElementById
-}
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
